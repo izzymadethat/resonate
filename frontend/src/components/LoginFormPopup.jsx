015dce1dff0b3059d270a2af01bfd9c0e9e1c014
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-12-01 18:27:43 -0500

    design login and sign up form

--- frontend/src/components/LoginFormPopup.jsx
+++ frontend/src/components/LoginFormPopup.jsx
@@ -23,32 +23,47 @@ function LoginFormModal() {
       });
   };
 
+  const demoLogin = () => {
+    setErrors({});
+    return dispatch(sessionActions.login({ credential: 'reso-nator', password: 'password' }))
+      .then(closeModal)
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) {
+          setErrors(data.errors);
+        }
+      });
+  };
+
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
+    <div className="relative w-full max-w-md p-12 space-y-6 min-h-80">
+      <h1 className="text-2xl font-bold"> Log In</h1 >
+      <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+        <label className="flex flex-col">
           Username or Email
           <input
             type="text"
             value={credential}
             onChange={(e) => setCredential(e.target.value)}
             required
+            className="w-full px-4 py-2 bg-transparent border rounded-md border-neutral-500 text-neutral-400"
           />
         </label>
-        <label>
+        <label className="flex flex-col">
           Password
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            className="w-full px-4 py-2 bg-transparent border rounded-md border-neutral-500 text-neutral-400"
             required
           />
         </label>
         {errors.credential && <p>{errors.credential}</p>}
-        <button type="submit">Log In</button>
+        <button type="submit" className="p-2 rounded-md bg-primary text-neutral-900 hover:bg-primary/90">Log In</button>
+        <button type="button" onClick={demoLogin}>Login as a Demo User</button>
       </form>
-    </>
+    </div >
   );
 }
 
--- frontend/src/components/SignupFormPopup.jsx
+++ frontend/src/components/SignupFormPopup.jsx
@@ -42,72 +42,82 @@ function SignupFormPopup() {
   };
 
   return (
-    <>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
+    <div className="relative w-full max-w-md p-12 space-y-6 min-h-80">
+      <h1 className="text-2xl font-bold">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+        <label className="flex flex-col">
           Email
           <input
-            type="text"
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
+            className="w-full px-4 py-2 bg-transparent border rounded-md border-neutral-500 text-neutral-400"
           />
         </label>
         {errors.email && <p>{errors.email}</p>}
-        <label>
+        <label className="flex flex-col">
           Username
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
+            className="w-full px-4 py-2 bg-transparent border rounded-md border-neutral-500 text-neutral-400"
           />
         </label>
         {errors.username && <p>{errors.username}</p>}
-        <label>
+        <label className="flex flex-col">
           First Name
           <input
             type="text"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
+            minLength={100}
             required
+            className="w-full px-4 py-2 bg-transparent border rounded-md border-neutral-500 text-neutral-400"
           />
         </label>
         {errors.firstName && <p>{errors.firstName}</p>}
-        <label>
+        <label className="flex flex-col">
           Last Name
           <input
             type="text"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
+            minLength={100}
             required
+            className="w-full px-4 py-2 bg-transparent border rounded-md border-neutral-500 text-neutral-400"
           />
         </label>
         {errors.lastName && <p>{errors.lastName}</p>}
-        <label>
+        <label className="flex flex-col">
           Password
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            minLength={6}
+            maxLength={20}
+            className="w-full px-4 py-2 bg-transparent border rounded-md border-neutral-500 text-neutral-400"
           />
         </label>
         {errors.password && <p>{errors.password}</p>}
-        <label>
+        <label className="flex flex-col">
           Confirm Password
           <input
             type="password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
+            className="w-full px-4 py-2 bg-transparent border rounded-md border-neutral-500 text-neutral-400"
           />
         </label>
         {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button type="submit">Sign Up</button>
+        <button type="submit" className="p-2 rounded-md bg-neutral-600 hover:bg-neutral-600/80">Sign Up</button>
       </form>
-    </>
+    </div>
   );
 }
 
