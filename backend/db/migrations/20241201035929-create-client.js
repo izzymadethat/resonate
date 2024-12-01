8b6653b6220dd778f65049066794267fd71f5ac8
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-12-01 00:39:16 -0500

    build client state

--- backend/db/migrations/20241201035929-create-client.js
+++ backend/db/migrations/20241201035929-create-client.js
@@ -1,47 +1,56 @@
 "use strict";
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Clients", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      projectId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Projects",
-          key: "id"
+    await queryInterface.createTable(
+      "Clients",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
         },
-        onDelete: "CASCADE"
+        projectId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Projects",
+            key: "id"
+          },
+          onDelete: "CASCADE"
+        },
+        name: {
+          type: Sequelize.STRING(100),
+          allowNull: false
+        },
+        email: {
+          type: Sequelize.STRING(256),
+          allowNull: false
+        },
+        avatar: {
+          type: Sequelize.STRING
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        }
       },
-      name: {
-        type: Sequelize.STRING(100),
-        allowNull: false
-      },
-      email: {
-        type: Sequelize.STRING(256),
-        allowNull: false
-      },
-      avatar: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      }
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Clients");
+    options.tableName = "Clients";
+    return queryInterface.dropTable("Clients");
   }
 };
---
+++ frontend/src/store/clients.js
@@ -0,0 +1,63 @@
+import { csrfFetch } from "./csrf";
+
+// const GET_PROJECT_CLIENTS = "clients/getProjectClients";
+// const GET_CLIENT = "clients/getClient";
+const ADD_CLIENT = "clients/addClient";
+const EDIT_CLIENT = "clients/editClient";
+const DELETE_CLIENT = "clients/deleteClient";
+
+const addClient = (client) => {
+  return {
+    type: ADD_CLIENT,
+    payload: client
+  };
+};
+
+const editClient = (client) => {
+  return {
+    type: EDIT_CLIENT,
+    payload: client
+  };
+};
+
+const deleteClient = (clientId) => {
+  return {
+    type: DELETE_CLIENT,
+    payload: clientId
+  };
+};
+
+export const fetchAddThunk = (projectId, client) => async (dispatch) => {
+  const response = await csrfFetch(`/api/projects/${projectId}/clients`, {
+    method: "POST",
+    body: JSON.stringify(client)
+  });
+  const data = await response.json();
+  dispatch(addClient(data));
+  return response;
+};
+
+export const fetchEditClient =
+  (projectId, clientId, client) => async (dispatch) => {
+    const response = await csrfFetch(
+      `/api/projects/${projectId}/clients/${clientId}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(client)
+      }
+    );
+    const data = await response.json();
+    dispatch(editClient(data));
+    return response;
+  };
+
+export const fetchDeleteClient = (projectId, clientId) => async (dispatch) => {
+  const response = await csrfFetch(
+    `/api/projects/${projectId}/clients/${clientId}`,
+    {
+      method: "DELETE"
+    }
+  );
+  dispatch(deleteClient(clientId));
+  return response;
+};
