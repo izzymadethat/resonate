4a0d2c277484dad308fc0b098c8bf1a84a843aba
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-12-06 09:12:04 -0500

    update homepage

--- frontend/src/components/OpenModalButton.jsx
+++ frontend/src/components/OpenModalButton.jsx
@@ -4,7 +4,8 @@ function OpenModalButton({
   modalComponent, // component to render inside the modal
   buttonText, // text of the button that opens the modal
   onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
-  onModalClose // optional: callback function that will be called once the modal is closed
+  onModalClose, // optional: callback function that will be called once the modal is closed
+  ...props
 }) {
   const { setModalContent, setOnModalClose } = useModal();
 
@@ -14,7 +15,11 @@ function OpenModalButton({
     if (typeof onButtonClick === "function") onButtonClick();
   };
 
-  return <button onClick={onClick}>{buttonText}</button>;
+  return (
+    <button onClick={onClick} className={props.className} {...props}>
+      {buttonText}
+    </button>
+  );
 }
 
 export default OpenModalButton;
